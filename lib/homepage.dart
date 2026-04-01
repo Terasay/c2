e743c1be5e5823fa055a2d5588d0fa74dce2b020
Terasay <limonorigin@gmail.com>
2026-04-01 12:18:08 +0500

    23637

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,12 +8,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _bottomNavIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome to the Homepage'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Row(
+        children: [
+          Container(
+            width: 120,
+            color: Colors.white,
+            child: const Center(child: Text('Category Menu')),
+          ),
+
+          const VerticalDivider(width: 1, thickness: 1, color: Colors.black12,),
+
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: const Center(child: Text('Main Content Area')),
+            ),
+          ),
+        ],
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _bottomNavIndex,
+        onTap: (index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+        ],
+      ),
+
     );
   }
 }
