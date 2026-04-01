2d94ccb29297c70f08d445f15e821b15a82a5e83
Terasay <limonorigin@gmail.com>
2026-04-01 12:34:12 +0500

    155

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,6 +9,38 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _bottomNavIndex = 1;
+  int _selectedCategoryIndex = 0;
+
+  Widget _buildCategoryItem(int index, String title, Widget iconWidget) {
+    bool isSelected = _selectedCategoryIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedCategoryIndex = index;
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          iconWidget,
+          
+          if (isSelected) ...[
+            const SizedBox(height: 8),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.orange,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +51,19 @@ class _HomePageState extends State<HomePage> {
           Container(
             width: 120,
             color: Colors.white,
-            child: const Center(child: Text('Category Menu')),
+            child: Column (
+              children: [
+                const SizedBox(height: 30),
+
+                _buildCategoryItem(0, 'Main dishes', Image.asset('assets/icons/icon_90.png', width: 24, height: 24)),
+                const SizedBox(height: 40),
+
+                _buildCategoryItem(1, 'Pasteries', Image.asset('assets/icons/icon_92.png', width: 24, height: 24)),
+                const SizedBox(height: 40),
+
+                _buildCategoryItem(2, 'Soups', Image.asset('assets/icons/icon_91.png', width: 24, height: 24)),
+              ],
+            ),
           ),
 
           const VerticalDivider(width: 1, thickness: 1, color: Colors.black12,),
